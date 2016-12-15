6eeab3e9eea509e89c9fb44ac5873c7848532dfe
rossi <rossi@10.192.100.247>
2016-12-15 13:23:27 +0100

    Michi

--- Bank/src/Test.java
+++ Bank/src/Test.java
@@ -3,5 +3,6 @@ public class Test {
  public static void main(String[] args) {
 	System.out.println("Hallo Fabio");
 	System.out.println("Timmy ist auch dabei");
+	System.out.println("Michi ist immernoch da");
 }
 }
