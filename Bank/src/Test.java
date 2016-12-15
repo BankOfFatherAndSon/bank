5e427f59307e3eafd20795c510f18471ac1751f9
icybermonkey <timiosoko@hotmail.com>
2016-12-15 13:34:41 +0100

    niggas

--- Bank/src/Test.java
+++ Bank/src/Test.java
@@ -5,5 +5,6 @@ public class Test {
 	System.out.println("Timmy ist auch dabei");
 	System.out.println("Michi ist immernoch da");
 	System.out.println("asdf");
+	System.out.println("Ich has glöscht und namal neu abeglade - Timi");
 }
 }
