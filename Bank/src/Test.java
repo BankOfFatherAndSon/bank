612a6be1b3e17a1eb765a5305e45cdb520614e82
Reto <Reto@10.192.100.166>
2016-12-15 13:47:44 +0100

    zum xte mal

--- Bank/src/Test.java
+++ Bank/src/Test.java
@@ -1,14 +1,11 @@
 
 public class Test {
- public static void main(String[] args) {
-	System.out.println("Hallo Fabio");
-	System.out.println("Timmy ist auch dabei");
-	System.out.println("Michi ist immernoch da");
-	System.out.println("asdf");
-	System.out.println("Ich has glöscht und namal neu abeglade - Timi");
-	System.out.println("dä hinderletscht isch au däbi");
-	
-	
-	
-}
+	public static void main(String[] args) {
+		System.out.println("Hallo Fabio");
+		System.out.println("Timmy ist auch dabei");
+		System.out.println("Michi ist immernoch da");
+		System.out.println("asdf");
+		System.out.println("Ich has glöscht und namal neu abeglade - Timi");
+		System.out.println("dä hinderletscht isch au däbi");
+	}
 }
