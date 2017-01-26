2ab6d6378802dcc7f3fbc77c81befdaaf97d68d7
icybermonkey <timiosoko@hotmail.com>
2017-01-26 14:02:18 +0100

    yooo

---
+++ Bank/src/ch/hwz/bfas/Aktien.java
@@ -0,0 +1,14 @@
+package ch.hwz.bfas;
+
+public class Aktien extends abstractPortfolio {
+
+	String isin, titelName, currency;
+	int valor;
+	double price, dividendPayment;
+	//date dividendDate;
+
+
+
+
+	
+}
---
+++ Bank/src/ch/hwz/bfas/abstractPortfolio.java
@@ -0,0 +1,15 @@
+package ch.hwz.bfas;
+
+abstract public class abstractPortfolio {
+
+	//Array
+	
+	
+	
+	
+	//Methods
+	addAktie(){
+		
+	}
+	
+}
